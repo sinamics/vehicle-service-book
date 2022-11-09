d07bb745fa2c8a6acd6e0d70f47a6e7b09ed7333
Jakub Soboczyński <teziovsky@gmail.com>
2022-11-09 16:10:23 +0100

    feat(packages): add superjson

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+const { withSuperjson } = require("next-superjson");
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: ["avatars.githubusercontent.com", "cloudflare-ipfs.com"],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withSuperjson()(nextConfig);
