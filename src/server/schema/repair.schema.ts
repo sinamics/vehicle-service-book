7de7d78d3d973ffeca6a7870c1d3572dd165c584
Jakub Soboczyński <teziovsky@gmail.com>
2022-12-03 21:54:10 +0100

    feat: update schemas

--- src/server/schema/repair.schema.ts
+++ src/server/schema/repair.schema.ts
@@ -8,25 +8,17 @@ export const repairParams = z.object({
 export const createRepairSchema = z.object({
   params: repairParams.pick({ carId: true }),
   body: z.object({
-    title: z.string({
-      required_error: "Title is required",
-    }),
-    description: z.string().optional().nullable(),
-    price: z.number().optional().nullable(),
-    date: z.string().optional().nullable(),
-    mileage: z.number().optional().nullable(),
+    title: z.string().min(3),
+    description: z.string().optional(),
+    price: z.number().nonnegative().optional(),
+    date: z.string().optional(),
+    mileage: z.number().nonnegative().optional(),
   }),
 });
 
 export const updateRepairSchema = z.object({
   params: repairParams,
-  body: z.object({
-    title: z.string().optional().nullable(),
-    description: z.string().optional().nullable(),
-    price: z.number().optional().nullable(),
-    date: z.string().optional().nullable(),
-    mileage: z.number().optional().nullable(),
-  }),
+  body: createRepairSchema.shape.body,
 });
 
 export type RepairParams = z.infer<typeof repairParams>;
