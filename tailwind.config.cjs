c097b5a2fae798647fc46a72ff1a307b1356660d
Jakub Soboczyński <teziovsky@gmail.com>
2022-12-04 15:19:09 +0100

    feat: add flowbite component library

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
   theme: {
     container: {
       center: true,
@@ -24,6 +27,7 @@ module.exports = {
     aspectRatio: false,
   },
   plugins: [
+    require("flowbite/plugin"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
