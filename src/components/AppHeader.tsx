25f7c63e89763c9eeb741dd81a09442ad139a72e
Jakub Soboczyński <teziovsky@gmail.com>
2022-12-27 20:09:41 +0100

    feat: init settings and profile views

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -116,7 +116,7 @@ export default function Header({ user }: Props) {
                         <Menu.Item>
                           {({ active }) => (
                             <Link
-                              href="/"
+                              href="/app/profile"
                               className={cx(
                                 "block px-4 py-2 text-sm text-gray-700 dark:text-gray-300",
                                 {
@@ -131,7 +131,7 @@ export default function Header({ user }: Props) {
                         <Menu.Item>
                           {({ active }) => (
                             <Link
-                              href="/"
+                              href="/app/settings"
                               className={cx(
                                 "block px-4 py-2 text-sm text-gray-700 dark:text-gray-300",
                                 {
