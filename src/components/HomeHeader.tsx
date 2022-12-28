5f23a3d50caec48995b0af54498812a027a44bc8
Jakub Soboczyński <teziovsky@gmail.com>
2022-12-28 10:57:00 +0100

    feat: add hover to logo in header

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -8,8 +8,8 @@ export default function Header() {
       <div className="container relative flex h-16 items-center justify-between px-8">
         <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-center">
           <div className="flex flex-shrink-0 items-center">
-            <Link className="flex items-center gap-3" href="/">
-              <Logo className="h-8 w-8" />
+            <Link className="group flex items-center gap-3" href="/">
+              <Logo className="h-8 w-8 group-hover:animate-spin-slow" />
               <div className="flex flex-col items-center">
                 <span className="text-xl leading-5 tracking-wide">Vehicle</span>
                 <span className="text-xs leading-[14px] tracking-tighter">
