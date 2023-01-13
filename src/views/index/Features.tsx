aa4ffa6d541a295b65a97e8c8ebeaf08604f6382
Jakub Soboczyński <teziovsky@gmail.com>
2023-01-13 17:05:21 +0100

    feature/tez-74-implement-feature-section

--- src/views/index/Features.tsx
+++ src/views/index/Features.tsx
@@ -1,60 +1,49 @@
-import Image from "next/image";
+import cx from "classnames";
+import { GiAutoRepair } from "react-icons/gi";
+import { ImStatsDots } from "react-icons/im";
+import { IoCarSportSharp } from "react-icons/io5";
 
 const features = [
   {
     id: 1,
-    title: "Elegant Dark Mode",
+    title: "Cars Management",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta maiores ratione quos omnis. Commodi, provident fugiat, repudiandae dolor repellat sapiente molestias dolores placeat corrupti dolore delectus velit nobis saepe error!",
-    icon: null,
+      "This feature allows users to add, edit and delete vehicle from the web application. This can include information such as make, model, production year, VIN, and many more.",
+    icon: <IoCarSportSharp className="h-8 w-8" />,
   },
   {
     id: 2,
-    title: "CEO of Company",
+    title: "Service and Repair Logging",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta maiores ratione quos omnis. Commodi, provident fugiat, repudiandae dolor repellat sapiente molestias dolores placeat corrupti dolore delectus velit nobis saepe error!",
-    icon: null,
+      "Allows users to log and keep track of all the service and repair history for each vehicle in the application. This can include information such as date, cost, mileage and details of the service performed.",
+    icon: <GiAutoRepair className="h-8 w-8" />,
   },
   {
     id: 3,
-    title: "CEO of Company",
+    title: "Statistics",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta maiores ratione quos omnis. Commodi, provident fugiat, repudiandae dolor repellat sapiente molestias dolores placeat corrupti dolore delectus velit nobis saepe error!",
-    icon: null,
-  },
-  {
-    id: 4,
-    title: "CEO of Company",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta maiores ratione quos omnis. Commodi, provident fugiat, repudiandae dolor repellat sapiente molestias dolores placeat corrupti dolore delectus velit nobis saepe error!",
-    icon: null,
-  },
-  {
-    id: 5,
-    title: "CEO of Company",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta maiores ratione quos omnis. Commodi, provident fugiat, repudiandae dolor repellat sapiente molestias dolores placeat corrupti dolore delectus velit nobis saepe error!",
-    icon: null,
+      "Allows users to generate various statistics and reports on the service history of all vehicles. This can include things like the total cost of repairs, total number of repairs, last mileage of repair, mileage between first and last repair and last repair date for each vehicle.",
+    icon: <ImStatsDots className="h-8 w-8" />,
   },
 ];
 
 export default function Features() {
   return (
     <section className="py-20 even:bg-base-200">
-      <div className="container mx-auto px-6 py-10">
-        <h1 className="text-3xl font-semibold capitalize text-gray-800 dark:text-white lg:text-4xl">
-          explore our awesome{" "}
-          <span className="underline decoration-secondary">Features</span>
-        </h1>
-        <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 xl:mt-12 xl:grid-cols-3 xl:gap-12">
-          {features.map((feature) => (
+      <div className="container mx-auto flex flex-col gap-12 px-6 py-10 md:gap-20">
+        <h2 className="text-center text-3xl font-black capitalize leading-10 text-accent md:text-5xl lg:text-6xl">
+          Explore our awesome{" "}
+          <span className="underline decoration-secondary">features</span>
+        </h2>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:mt-12 xl:grid-cols-3 xl:gap-12">
+          {features.map((feature, index) => (
             <div
               key={feature.id}
-              className="space-y-3 rounded-xl border-2 border-accent p-8"
+              className={cx("space-y-3 rounded-xl border-2 border-accent p-8", {
+                "md:col-span-2 xl:col-span-1": index === features.length - 1,
+              })}
             >
-              <span className="inline-block text-blue-500 dark:text-blue-400">
-                {feature.icon}
-              </span>
+              <span className="inline-block text-accent">{feature.icon}</span>
               <h1 className="text-2xl font-semibold capitalize text-gray-700 dark:text-white">
                 {feature.title}
               </h1>
