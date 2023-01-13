575fcf644c1a5141f4938f565adf79b757a54926
Jakub Soboczyński <teziovsky@gmail.com>
2023-01-13 17:04:56 +0100

    feat: Implement numbers section TEZ-75

--- src/views/index/Numbers.tsx
+++ src/views/index/Numbers.tsx
@@ -1,23 +1,23 @@
 const numbers = [
   {
     id: 1,
-    label: "Cars",
-    value: "500+",
-  },
-  {
-    id: 2,
     label: "Repairs",
     value: "20k+",
   },
   {
+    id: 2,
+    label: "Cars",
+    value: "1k+",
+  },
+  {
     id: 3,
-    label: "John Doe",
-    value: "300",
+    label: "Saved hours",
+    value: "800+",
   },
   {
     id: 4,
-    label: "John Doe",
-    value: "25+",
+    label: "Users",
+    value: "300+",
   },
 ];
 
@@ -28,29 +28,25 @@ export default function Numbers() {
         <div className="container mx-auto flex w-full flex-col items-center justify-center">
           <div className="flex flex-col items-center justify-center">
             <div className="mt-20">
-              <h2 className="text-center text-4xl font-black leading-10 text-white md:text-5xl lg:text-6xl">
-                By the numbers
+              <h2 className="text-center text-3xl font-black capitalize leading-10 text-accent md:text-5xl lg:text-6xl">
+                By the{" "}
+                <span className="underline decoration-secondary">numbers</span>
               </h2>
-            </div>
-            <div className="mx-2 mt-6 text-center md:mx-0">
-              <p className="text-sm leading-6 text-white md:text-base  lg:text-lg">
-                5 years, consistent quality and you get results. No matter what
-              </p>
             </div>
           </div>
         </div>
       </div>
       <div className="container mx-auto -mt-8 flex items-center justify-center">
-        <div className="grid gap-x-2 gap-y-2 sm:grid-cols-2 md:gap-x-6 md:gap-y-6 lg:grid-cols-4 lg:gap-x-6">
+        <div className="grid gap-2 sm:grid-cols-2 sm:gap-8 md:gap-10 lg:grid-cols-4 lg:gap-12">
           {numbers.map((number, index) => (
             <div
               key={index}
-              className="flex h-40 w-40 flex-col items-center justify-center rounded-2xl bg-white shadow sm:h-48 sm:w-48 md:h-52 md:w-52 lg:h-56 lg:w-56"
+              className="flex h-40 w-40 flex-col items-center justify-center rounded-2xl bg-white shadow transition-transform sm:h-48 sm:w-48 sm:even:translate-y-8 sm:even:transform sm:hover:-translate-y-4 sm:hover:even:translate-y-4 md:h-52 md:w-52 lg:h-56 lg:w-56"
             >
               <h2 className="text-center text-2xl font-extrabold leading-10 text-gray-800 md:text-4xl lg:text-5xl">
                 {number.value}
               </h2>
-              <p className="mt-4 text-center text-sm leading-none text-gray-600 md:text-base lg:text-lg">
+              <p className="mt-4 text-center text-base leading-none text-gray-600 md:text-lg lg:text-xl">
                 {number.label}
               </p>
             </div>
