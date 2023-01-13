d529425935a7ce354734eea1fdcac46549165695
Jakub Soboczyński <teziovsky@gmail.com>
2023-01-13 17:04:12 +0100

    feat: implement testimonials section TEZ-76

--- src/views/index/Testimonials.tsx
+++ src/views/index/Testimonials.tsx
@@ -1,4 +1,4 @@
-import { useAutoAnimate } from "@formkit/auto-animate/react";
+import cx from "classnames";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -41,10 +41,9 @@ const testimonials = [
 ];
 
 export default function Testimonials() {
-  const [testimonialsContainer] = useAutoAnimate<HTMLDivElement>();
   const [index, setIndex] = useState(0);
 
-  const activeTestimonial = testimonials[index];
+  const activeTestimonial = index >= 0 ? testimonials[index] : null;
 
   useEffect(() => {
     const carousel = setInterval(() => {
@@ -52,7 +51,7 @@ export default function Testimonials() {
         if (prevIndex === testimonials.length - 1) return 0;
         return prevIndex + 1;
       });
-    }, 7000);
+    }, 10000);
 
     return () => clearTimeout(carousel);
   }, []);
@@ -60,29 +59,49 @@ export default function Testimonials() {
   if (!activeTestimonial) return null;
 
   return (
-    <section className="py-20 even:bg-base-200">
+    <section className="py-32 even:bg-base-200">
       <div className="container">
-        <div
-          ref={testimonialsContainer}
-          className="mx-auto flex min-h-[300px] max-w-xl flex-col items-center justify-center"
-        >
-          <p className="mb-6 text-center font-serif text-base italic text-accent sm:text-xl md:text-2xl">
-            &quot;{activeTestimonial.text}&quot;
-          </p>
-          <div className="flex flex-col items-center justify-center">
-            <Image
-              src={activeTestimonial.avatar}
-              alt={activeTestimonial.name}
-              width={64}
-              height={64}
-              className="mb-2 h-12 w-12 rounded-full sm:h-14 sm:w-14 md:h-16 md:w-16"
-            />
-            <h3 className="text-sm font-bold text-accent sm:text-base md:text-lg">
-              {activeTestimonial.name}
-            </h3>
-            <p className="text-center text-xs font-medium text-gray-400 sm:text-sm md:text-base">
-              {activeTestimonial.title}
-            </p>
+        <div className="mx-auto flex min-h-[300px] max-w-xl flex-col items-center justify-center gap-6">
+          <h2 className="mb-4 text-center text-4xl font-black capitalize leading-10 text-accent md:text-5xl lg:text-6xl">
+            Testimonials
+          </h2>
+          {activeTestimonial ? (
+            <>
+              <p className="text-center font-serif text-base italic text-accent sm:text-xl md:text-2xl">
+                &quot;{activeTestimonial.text}&quot;
+              </p>
+              <div className="flex flex-col items-center justify-center">
+                <Image
+                  src={activeTestimonial.avatar}
+                  alt={activeTestimonial.name}
+                  width={64}
+                  height={64}
+                  className="mb-2 h-12 w-12 rounded-full sm:h-14 sm:w-14 md:h-16 md:w-16"
+                />
+                <h3 className="text-sm font-bold text-accent sm:text-base md:text-lg">
+                  {activeTestimonial.name}
+                </h3>
+                <p className="text-center text-xs font-medium text-accent/60 sm:text-sm md:text-base">
+                  {activeTestimonial.title}
+                </p>
+              </div>
+            </>
+          ) : null}
+          <div className="flex gap-2">
+            {testimonials.map((_, idx) => (
+              <button
+                className={cx(
+                  "h-6 w-6 before:block before:h-1 before:w-6 before:rounded-full before:transition-colors before:content-[''] hover:before:bg-accent",
+                  {
+                    "before:bg-primary": idx !== index,
+                    "before:bg-secondary": idx === index,
+                  }
+                )}
+                aria-label={`Show testimonial number ${idx + 1}`}
+                onClick={() => setIndex(idx)}
+                key={idx}
+              />
+            ))}
           </div>
         </div>
       </div>
